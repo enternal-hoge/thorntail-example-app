5892f1e53e7b6903a2b509f79634546cd7e21c99
Kenichi Murakata <43528443+enternal-hoge@users.noreply.github.com>
2019-02-24 14:18:44 +0900

    Add files via upload

---
+++ src/main/java/eternal/hoge/thorntail/simple/example/MyApplication.java
@@ -0,0 +1,12 @@
+package eternal.hoge.thorntail.simple.example;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+
+@ApplicationPath("/")
+public class MyApplication extends Application {
+
+    public MyApplication() {
+    }
+}
